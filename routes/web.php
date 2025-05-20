459f0b913a0c113a2912a0fac6a773ce4f34177c
Rafi123-CMD <rafiulhuda27@gmail.com>
2025-05-20 13:05:18 +0700

    menampilkan detail menu di fe

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -8,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/{username}', [FrontendController::class, 'index'])->name('index');
+Route::get('/{username}/product/{id}', [ProductController::class, 'show'])->name('product.show');
