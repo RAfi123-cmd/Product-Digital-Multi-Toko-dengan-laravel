3485fa375505fc940fa58e472365a69d349d7353
Rafi123-CMD <rafiulhuda27@gmail.com>
2025-05-21 12:09:05 +0700

    cart

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -13,3 +14,5 @@ Route::get('/{username}', [FrontendController::class, 'index'])->name('index');
 Route::get('/{username}/find-product', [ProductController::class, 'find'])->name('product.find');
 Route::get('/{username}/find-product/results', [ProductController::class, 'findResults'])->name('product.find-results');
 Route::get('/{username}/product/{id}', [ProductController::class, 'show'])->name('product.show');
+
+Route::get('/{username}/cart', [TransactionController::class, 'cart'])->name('cart');
